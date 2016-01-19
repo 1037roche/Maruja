e27b14125aa213dbf1b74877ea08268d4668e6e3
sebastian dela roche <sebastian dela roche>
2016-01-19 06:04:03 +0100

    ...

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
 module.exports = require('./maruja');
+
+module.exports.stream();
--- maruja.js
+++ maruja.js
@@ -3,14 +3,51 @@ var moment = require('moment');
 
 var maruja = module.exports = {};
 
-maruja.stream = function () {};
+maruja.stream = function (config) {
+
+	config = config || {};
+	config.file = config.file || 'node.log';
+
+	// existence of the file
+	if(!existFile(config.file))
+	{
+		createFile(config.file, 'texto de pruebas');
+	}
 
 /*
-var maruja = require('maruja');
-maruja.stream({
-	filepath : __dirname + '/log',
-	filename : 'access',
-	size : "20 MB",
-	maxfile : 20
-});
+	fs.readFile(config.file, 'utf8', function (err,data) {
+	  if (err) {
+	    return console.log(err);
+	  }
+	  console.log(data);
+	});
+
 */
+		var stat =  fs.statSync(config.file);
+		console.log(stat);
+
+
+};
+
+function existFile(file) {
+    try
+    {
+        return fs.statSync(file).isFile();
+    }
+    catch (err)
+    {
+    	return false;
+    }
+}
+
+function createFile(nameFile, text) {
+	try
+	{
+		fs.writeFile(nameFile, text);
+		return true;
+	}
+	catch(e)
+	{
+		return false;
+	}
+}
