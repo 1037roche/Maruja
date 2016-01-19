92baffe1c70394fe18f80d788a73a6a7c94f2bfa
Jonattan Velasquez <jonattanva89@gmail.com>
2016-01-18 22:57:43 -0500

    create method stream

--- maruja.js
+++ maruja.js
@@ -1,2 +1,16 @@
 var fs = require('fs');
 var moment = require('moment');
+
+var maruja = module.exports = {};
+
+maruja.stream = function () {};
+
+/*
+var maruja = require('maruja');
+maruja.stream({
+	filepath : __dirname + '/log',
+	filename : 'access',
+	size : "20 MB",
+	maxfile : 20
+});
+*/
